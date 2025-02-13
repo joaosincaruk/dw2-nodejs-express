a6dc88aaec05715b481a2ebb74da2403cbe9e91d
joaosincaruk <vieira.joaovic@gmail.com>
2025-02-13 19:01:49 -0300

    Variáveis de JS joao

---
+++ aula-0.1-variaveis-e-funcoes/js-aula-0.1.js
@@ -0,0 +1,23 @@
+//Comentario em JS
+/* comentario em bloco
+...
+*/
+
+//Declaraçao de variaveis no JS
+//CONST -  LET - VAR
+//CONST nao muda valor, LET valor muda mas nao redeclara, VAR altera
+//evitar o uso de var por segurança 
+//no escopo GLOBAL, const nao pode ser alterada
+
+const nome = "João" // precisa de valor inicial, por isso deu erro antes de declarar
+let Nome // permite iniciar a variavel vazia
+//const nome = "Pedro" //Resulta em erro, pois nome ja foi declarado
+
+
+var cor = "Azul"
+var cor = "Amarelo"
+let cidade = "Registro"
+cidade = "Jacupiranga" // Let permite alterar o valor
+
+const message = "Hello, World! Iniciando estudos em JS"
+console.log(message)
